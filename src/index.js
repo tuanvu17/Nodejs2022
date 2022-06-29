4b86af68bd304b4b0850b0d23b9f2046067e2001
tuanvu17 <tuanvu17mta@gmail.com>
2022-06-29 19:37:31 +0700

    cai dat scss

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+
+// path ==>> show img
+app.use(express.static(path.join(__dirname,'public')));
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template Engine 
+app.engine('hbs',handlebars.engine({
+  extname: '.hbs'
+}));
+
+
+app.set('view engine','hbs');
+app.set('views', path.join(__dirname, 'resources/views/'));
+
+console.log(__dirname)
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
